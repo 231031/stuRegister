5f10867d2f2d6ee5acc8f31ee810718bbe48ad46
suneenad <suneenad.sang@mail.kmutt.ac.th>
2024-05-03 23:33:27 +0700

    Adding Coral to the color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,7 +13,8 @@ export default {
       'lowyellow': '#FFF5E0',
       'lowgreen': '8DECB4',
       'black': '#000000',
-      'white': '#FFF7FC'
+      'white': '#FFF7FC',
+      'coral': '#FF7F50',
     },},
   },
   plugins: [],

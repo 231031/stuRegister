9af9a9e3dae4ae49888067c074ed75224d5fc40c
PRAEWA CHOOBANNA <102781668+praewery@users.noreply.github.com>
2024-05-06 23:53:47 +0700

    add dropdown CHOOSE A SCHOLARSHIP in scholarshipform page

--- client/src/components/Student/StudentScholarform.jsx
+++ client/src/components/Student/StudentScholarform.jsx
@@ -138,6 +138,8 @@ export default function Scholarform() {
                 </div>
               </div>
             </div>
+
+
           </div>
 
 
@@ -300,6 +302,30 @@ export default function Scholarform() {
         <label className="block uppercase tracking-wide text-gray-700 text-xl font-bold mb-2" htmlFor="Agent Register">
         file graduation certificate
          </label>
+
+         <div className="w-300px px-3 relative">
+    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-salary">
+    Choose a scholarship
+    </label>
+    <div className="relative">
+        <select 
+            name="salary"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.salary}
+            className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+            <option value='Oxbridge University'>Oxbridge University</option>
+            <option value='michigan University '>michigan University</option>
+            <option value='Pacific University'> michigan University</option>
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+            </svg>
+        </div>
+    </div>
+</div>
+
 
         <div className="mt-8 mb-8">
           <input type="file" name="file" id="file" className="sr-only" />

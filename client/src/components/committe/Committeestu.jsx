3ac781e8fe2280376113adef404780716f3f20b8
231031 <mybile.e70e@gmail.com>
2024-05-13 15:34:22 +0700

    committeestuinfo

--- client/src/components/committe/Committeestu.jsx
+++ client/src/components/committe/Committeestu.jsx
@@ -2,30 +2,38 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
-import { getInfoStudent } from "../../helpers/comHelper";
+import { getInfoStudent } from "../../helpers/comHelper"
+import Headercom from "./Headercom";
 
 export default function Committeestu() {
   
   const navigate = useNavigate();
   const location = useLocation();
-  const [stu, setStu] = useState("");
+  const [info, setInfo] = useState("");
+  const [gpax, setGpax] = useState("");
   const [id, setId] = useState("");
+  const [scholar, setScholar] = useState("");
 
   useEffect(() => {
-
-    if (location.state) setId(location.state.id);
+    if (location.state) {
+      setId(location.state.student_id);
+      setScholar(location.state.scholarship_id);
+    }
     else navigate('/committee/tablestu');
+  }, []);
 
+  useEffect(() => {
     const apiStu = async () => {
       try {
         const res = await getInfoStudent(id);
-        setStu(res);
+        setInfo(res.info);
+        setGpax(res.gpax);
       } catch (error) {
         console.error(error);
       }
     };
     if (id) apiStu();
-  }, []);
+  }, [id]);
 
 
   return (
@@ -34,7 +42,85 @@ export default function Committeestu() {
       <Helmet>
           <title>C | Student</title>
       </Helmet>
+      <Headercom/>
+      <div className="flex flex-col items-center my-10">
+        <div className="flex flex-col flex-wrap w-2/3 h-2/3 bg-Slate rounded-md shadow-md p-5">
 
+          {/* header */}
+          <div className="flex flex-row mt-5">
+            <p className="font-bold mr-5">Applicant of Scholarship :</p>
+            <p className="mr-8">{scholar}</p>
+          </div>
+
+          {/* personal */}
+          <div className="my-5">
+            <h3 className="my-2 font-extrabold">Personal</h3>
+            {/* row 1 */}
+            <div className="flex flex-row">
+              <div className="flex flex-row">
+                <p className="font-bold mr-5">Student ID :</p>
+                <p className="mr-8">{info?.student_id}</p>
+              </div>    
+              <div className="flex flex-row">
+                <p className="font-bold mr-5">First Name :</p>
+                <p className="mr-8">{info?.first_name}</p>
+              </div>    
+              <div className="flex flex-row">
+                <p className="font-bold mr-5">Last Name :</p>
+                <p>{info?.last_name}</p>
+              </div>
+            </div>
+            {/* row 2 */}
+            <div className="flex flex-row">
+              <p className="font-bold mr-5">Salary :</p>
+              <p>{info?.salary}</p>
+            </div>
+            {/* row 3 */}
+            <div className="flex flex-row">
+              <p className="font-bold mr-5">Gpax :</p>
+              <p>{gpax}</p>
+            </div>
+          </div>
+          
+          {/* family */}
+          <div className="my-5">
+            <h3 className="my-2 font-extrabold">Family</h3>
+            <div className="flex flex-row">
+              <p className="font-bold mr-5">Father Salary :</p>
+              <p>{info?.f_salary}</p>
+            </div>
+            <div className="flex flex-row">
+              <p className="font-bold mr-5">Mother Salary :</p>
+              <p>{info?.m_salary}</p>
+            </div> 
+          </div>
+
+          {/* address */}
+          <div className="my-5">
+            <h3 className="my-2 font-extrabold">Present Address</h3>
+            <div className="flex flex-row">
+              <div className="flex flex-row mr-8">
+                <p className="font-bold mr-5">Address :</p>
+                <p>{info?.address}</p>
+              </div>
+              <div className="flex flex-row mr-8">
+                <p className="font-bold mr-5">City :</p>
+                <p>{info?.city}</p>
+              </div>
+              <div className="flex flex-row mr-8">
+                <p className="font-bold mr-5">State :</p>
+                <p>{info?.state}</p>
+              </div>
+              <div className="flex flex-row mr-8">
+                <p className="font-bold mr-5">Zip Code :</p>
+                <p>{info?.zip_code}</p>
+              </div>
+            </div>
+            
+          </div> 
+        </div>
+      </div>
+      
     </div>
     </HelmetProvider>
   )

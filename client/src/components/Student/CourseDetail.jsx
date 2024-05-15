461cab73d694310ef6070984230f7a4be846cecb
Petch <arthit.nop@gmail.com>
2024-05-16 00:35:22 +0700

    course detail

--- client/src/components/Student/CourseDetail.jsx
+++ client/src/components/Student/CourseDetail.jsx
@@ -3,11 +3,10 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
-
 import Headerstu from './Headerstu';
-
+import imageU1_2 from '../../assets/course.jpg';
+import image_3 from '../../assets/profile.png';
 export default function Detailcourse() {
-
     const navigate = useNavigate();
     const [data, setData] = useState('');
     useEffect(() => {
@@ -22,19 +21,22 @@ export default function Detailcourse() {
           student_id: student_id,
         });
       }, []);
-
   return (
     <HelmetProvider>
       <div>
         <Toaster position='top-center' reverseOrder={false}></Toaster>
         <Helmet>
-            <title>Stu | CourseDetail</title>
+        <title>Stu | CourseDetail</title>
         </Helmet> 
         <Headerstu data={data}/>
-        <div className='h-screen bg-white'>
+        <div className='h-screen bg-white '>
+        <div className=''> 
+          <div className='px-20 pt-20 pb-3 font-bold text-4xl'>Published Classes</div>
+          <div className='px-20 text-xl'>These classes are available to students.</div>
+        </div>
         <div className='flex flex-row justify-start items-start space-x-10 px-20 font-semibold'>
-          <div className='size-48 my-10 flex flex-col justify-end bg-sky rounded-3xl '>
-              <button className='flex flex-col w-100% bg-darkgreen h-2/5 rounded-b-3xl '>
+          <div className='size-48 my-10 flex flex-col justify-end  rounded-3xl bg-contain ' style={{backgroundImage: `url(${imageU1_2})`}}>
+              <button className='flex flex-col w-100% bg-lowbrown h-2/5 rounded-b-3xl '>
                 <div className='text-white ml-4 mt-2'>
                   CPE : 232
                 </div>
@@ -43,8 +45,8 @@ export default function Detailcourse() {
                 </div>
               </button>
           </div>
-          <div className='size-48 my-10 flex flex-col justify-end bg-sky rounded-3xl'>
-              <button className='flex flex-col w-100% bg-darkgreen h-2/5 rounded-b-3xl'>
+          <div className='size-48 my-10 flex flex-col justify-end rounded-3xl bg-contain' style={{backgroundImage: `url(${imageU1_2})`}}>
+              <button className='flex flex-col w-100% bg-lowbrown h-2/5 rounded-b-3xl'>
                 <div className='text-white ml-4 mt-2'>
                   CPE : 232
                 </div>
@@ -53,7 +55,7 @@ export default function Detailcourse() {
                 </div>
               </button>
           </div>
-          <div className='size-48 my-10 flex flex-col justify-end bg-sky rounded-3xl'>
+          <div className='size-48 my-10 flex flex-col justify-end  rounded-3xl bg-contain' style={{backgroundImage: `url(${imageU1_2})`}}>
               <button className='flex flex-col w-100% bg-lowbrown h-2/5 rounded-b-3xl'>
                 <div className='text-white ml-4 mt-2'>
                   CPE : 232
